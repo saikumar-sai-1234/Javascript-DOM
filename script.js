3fbc04004b80cffc0b4aeb5e19336fb918e6e6fd
saikumark <kolichelimisaikumar9@gmail.com>
2022-08-23 07:23:10 +0530

    index

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.getElementById("p1").innerHTML = "New text!";
+// document.body.style.background = 'red';
+
+
+function myFunction(p1,p2){
+    return p1*p2;
+}
+document.getElementById("demo").innerHTML=myFunction(4,3);
